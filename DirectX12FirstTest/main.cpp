65879e2fbfb714ce630686f3f7965fe1173da739
Sunday14 <18268878136@163.com>
2020-11-21 23:22:44 +0800

    display a simpe window

--- DirectX12FirstTest/main.cpp
+++ DirectX12FirstTest/main.cpp
@@ -1,7 +1,9 @@
 #define WIN32_LEAN_AND_MEAN
-#include<Windows.h>
-#include<shellapi.h>
+#include <Windows.h>
+#include <shellapi.h> // For CommandLineToArgvW
 
+// The min/max macros conflict with like-named member functions.
+// Only use std::min and std::max defined in <algorithm>.
 #if defined(min)
 #undef min
 #endif
@@ -10,42 +12,50 @@
 #undef max
 #endif
 
+// In order to define a function called CreateWindow, the Windows macro needs to
+// be undefined.
 #if defined(CreateWindow)
 #undef CreateWindow
 #endif
 
-#include<wrl.h>
-
-#include <d3d12.h>
-#include<dxgi1_6.h>
-#include<d3dcompiler.h>
-#include<DirectXMath.h>
-
-#include "d3dx12.h"
-
-#include<algorithm>
-#include<cassert>
-#include <chrono>
-
-#include"Helpers.h"
-
+// Windows Runtime Library. Needed for Microsoft::WRL::ComPtr<> template class.
+#include <wrl.h>
 using namespace Microsoft::WRL;
 
+// DirectX 12 specific headers.
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+// D3D12 extension library.
+#include"d3dx12.h"
+
+// STL Headers
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+
+// Helper functions
+#include "Helpers.h"
+
+// The number of swap chain back buffers.
 const uint8_t g_NumFrames = 3;
-
-
-//use a software rasterizer (Windows Advanced Rasterization Platform - WARP) https://docs.microsoft.com/en-us/windows/win32/direct3darticles/directx-warp?redirectedfrom=MSDN
+// Use WARP adapter
 bool g_UseWarp = false;
 
 uint32_t g_ClientWidth = 1280;
 uint32_t g_ClientHeight = 720;
 
+// Set to true once the DX12 objects have been initialized.
 bool g_IsInitialized = false;
 
+// Window handle.
 HWND g_hWnd;
-
+// Window rectangle (used to toggle fullscreen state).
 RECT g_WindowRect;
 
+// DirectX 12 Objects
 ComPtr<ID3D12Device2> g_Device;
 ComPtr<ID3D12CommandQueue> g_CommandQueue;
 ComPtr<IDXGISwapChain4> g_SwapChain;
@@ -53,7 +63,6 @@ ComPtr<ID3D12Resource> g_BackBuffers[g_NumFrames];
 ComPtr<ID3D12GraphicsCommandList> g_CommandList;
 ComPtr<ID3D12CommandAllocator> g_CommandAllocators[g_NumFrames];
 ComPtr<ID3D12DescriptorHeap> g_RTVDescriptorHeap;
-
 UINT g_RTVDescriptorSize;
 UINT g_CurrentBackBufferIndex;
 
@@ -71,6 +80,7 @@ bool g_TearingSupported = false;
 // Can be toggled with the Alt+Enter or F11
 bool g_Fullscreen = false;
 
+// Window callback function.
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 void ParseCommandLineArguments()
@@ -115,23 +125,22 @@ void RegisterWindowClass(HINSTANCE hInst, const wchar_t* windowClassName)
 	// Register a window class for creating our render window with.
 	WNDCLASSEXW windowClass = {};
 
-	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.cbSize = sizeof(WNDCLASSEXW);
 	windowClass.style = CS_HREDRAW | CS_VREDRAW;
 	windowClass.lpfnWndProc = &WndProc;
 	windowClass.cbClsExtra = 0;
 	windowClass.cbWndExtra = 0;
 	windowClass.hInstance = hInst;
-	windowClass.hIcon = ::LoadIcon(hInst, NULL);
+	windowClass.hIcon = ::LoadIcon(hInst, NULL); //  MAKEINTRESOURCE(APPLICATION_ICON));
 	windowClass.hCursor = ::LoadCursor(NULL, IDC_ARROW);
 	windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 	windowClass.lpszMenuName = NULL;
 	windowClass.lpszClassName = windowClassName;
-	windowClass.hIconSm = ::LoadIcon(hInst, NULL);
+	windowClass.hIconSm = ::LoadIcon(hInst, NULL); //  MAKEINTRESOURCE(APPLICATION_ICON));
 
-	static ATOM atom = ::RegisterClassExW(&windowClass);
-	assert(atom > 0);
+	static HRESULT hr = ::RegisterClassExW(&windowClass);
+	assert(SUCCEEDED(hr));
 }
-
 
 HWND CreateWindow(const wchar_t* windowClassName, HINSTANCE hInst,
 	const wchar_t* windowTitle, uint32_t width, uint32_t height)
@@ -169,3 +178,88 @@ HWND CreateWindow(const wchar_t* windowClassName, HINSTANCE hInst,
 	return hWnd;
 }
 
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		break;
+	}
+
+	return DefWindowProc(hwnd, message, wParam, lParam);
+}
+
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
+{
+	// Windows 10 Creators update adds Per Monitor V2 DPI awareness context.
+	// Using this awareness context allows the client area of the window 
+	// to achieve 100% scaling while still allowing non-client window content to 
+	// be rendered in a DPI sensitive fashion.
+	SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+
+	// Window class name. Used for registering / creating the window.
+	const wchar_t* windowClassName = L"DX12WindowClass";
+	ParseCommandLineArguments();
+	EnableDebugLayer();
+
+	//g_TearingSupported = CheckTearingSupport();
+
+	RegisterWindowClass(hInstance, windowClassName);
+	g_hWnd = CreateWindow(windowClassName, hInstance, L"Cearning DirectX 12",
+		g_ClientWidth, g_ClientHeight);
+
+	// Initialize the global window rect variable.
+	::GetWindowRect(g_hWnd, &g_WindowRect);
+
+	//ComPtr<IDXGIAdapter4> dxgiAdapter4 = GetAdapter(g_UseWarp);
+
+	//g_Device = CreateDevice(dxgiAdapter4);
+
+	//g_CommandQueue = CreateCommandQueue(g_Device, D3D12_COMMAND_LIST_TYPE_DIRECT);
+
+	//g_SwapChain = CreateSwapChain(g_hWnd, g_CommandQueue,
+		//g_ClientWidth, g_ClientHeight, g_NumFrames);
+
+	/*g_CurrentBackBufferIndex = g_SwapChain->GetCurrentBackBufferIndex();
+
+	g_RTVDescriptorHeap = CreateDescriptorHeap(g_Device, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, g_NumFrames);
+	g_RTVDescriptorSize = g_Device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+	UpdateRenderTargetViews(g_Device, g_SwapChain, g_RTVDescriptorHeap);*/
+
+	for (int i = 0; i < g_NumFrames; ++i)
+	{
+		//g_CommandAllocators[i] = CreateCommandAllocator(g_Device, D3D12_COMMAND_LIST_TYPE_DIRECT);
+	}
+	//g_CommandList = CreateCommandList(g_Device,
+		//g_CommandAllocators[g_CurrentBackBufferIndex], D3D12_COMMAND_LIST_TYPE_DIRECT);
+
+	//g_Fence = CreateFence(g_Device);
+	//g_FenceEvent = CreateEventHandle();
+
+	g_IsInitialized = true;
+
+	::ShowWindow(g_hWnd, SW_SHOW);
+
+	MSG msg = {};
+	while (msg.message != WM_QUIT)
+	{
+		if (::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
+		}
+	}
+
+	// Make sure the command queue has finished all commands before closing.
+	//Flush(g_CommandQueue, g_Fence, g_FenceValue, g_FenceEvent);
+
+	::CloseHandle(g_FenceEvent);
+
+	return 0;
+}
